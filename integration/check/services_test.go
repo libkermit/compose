8daad49f03718b5c67727439078224cf7e6f556a
nmengin <nicolas@containo.us>
2017-09-06 14:37:47 +0200

    Report services management to check package

---
+++ integration/check/services_test.go
@@ -0,0 +1,29 @@
+package composeit
+
+import (
+	"testing"
+
+	"github.com/go-check/check"
+	compose "github.com/libkermit/compose/check"
+)
+
+// Hook up gocheck into the "go test" runner
+func TestServices(t *testing.T) { check.TestingT(t) }
+
+type CheckServicesSuite struct{}
+
+var _ = check.Suite(&CheckServicesSuite{})
+
+func (s *CheckServicesSuite) TestServicesProject(c *check.C) {
+	project := compose.CreateProject(c, "services", "./assets/services.yml")
+	project.Start(c, "hello")
+
+	container := project.Container(c, "hello")
+	c.Assert(container.Name, check.Equals, "/services_hello_1")
+
+	//"No container found for '%s' service
+	project.NoContainer(c, "other")
+
+	project.Stop(c, "hello")
+
+}
